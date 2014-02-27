a7903e0c76879911fcf6075efbcdb334052dedf4
CHEN Qing <qing.chen@rea-group.com>
2014-02-27 11:21:35 +0800

    add some log output.

--- s3-upload.rb
+++ s3-upload.rb
@@ -3,18 +3,33 @@
 require 'rubygems'
 require 'aws-sdk'
 
+puts 'config aws with access account'
 AWS.config(
     :access_key_id => 'AKIAJ6ZQ3YPWY7QQPQ2Q',
     :secret_access_key => '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'
 )
+puts 'config finished'
 
-bucket_name = 'aws-training-s3-upload-day3'
+bucket_name = 'aws-training-s3-upload-day3-1'
 file_name = '500MB-file'
 
-# Get an instance of the S3 interface.
 s3 = AWS::S3.new
+puts "Get an instance of the S3 interface."
 
-# Upload a file.
+puts "remove bucket #{bucket_name} if exist"
+
+bucket = s3.buckets[bucket_name]
+if bucket.exists?
+  bucket.delete
+end
+
+puts 'Uploading a file.'
 key = File.basename(file_name)
-s3.buckets[bucket_name].objects[key].write(:file => file_name)
-puts "Uploading file #{file_name} to bucket #{bucket_name}."
+puts "object key is #{key}"
+puts "create bucket"
+bucket = s3.buckets.create(bucket_name)
+puts "bucket #{bucket_name} is created "
+
+puts "uploading object"
+bucket.objects[key].write(:file => file_name)
+puts "Object file #{file_name} has uploaded to #{bucket_name}"
